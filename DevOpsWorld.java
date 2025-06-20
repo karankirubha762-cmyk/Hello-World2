7d965f12d538e670f1be5e898acd5be3811cf72d
Srinivasa1999 <bsreddy2424@gmail.com>
2025-06-20 11:30:08 +0530

    Update DevOpsWorld.java

--- DevOpsWorld.java
+++ DevOpsWorld.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
  * DevOpsWorld is a basic Java program designed to demonstrate
  * good practices in naming, input validation, and maintainability.
  */
-public class DevOpsWorld {
+public class devOpsworld {
 
     /**
      * The main method is the entry point of the application.
