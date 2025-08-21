c73fbb7bdb4af9c450b5974814c295d4ee7e1df7
2107083 <deb.nath.3.1416@gmail.com>
2025-08-21 21:04:33 +0600

    After the 3rd step

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,13 @@ public class HelloWorld {
         Student s = new Student("Alice", 20);
         System.out.println("Student Name: " + s.getName());
         System.out.println("Student Age: " + s.getAge());
+
+        System.out.println("2 + 3 = " + Calculator.add(2, 3));
+        System.out.println("7 - 4 = " + Calculator.subtract(7, 4));
+        System.out.println("6 * 5 = " + Calculator.multiply(6, 5));
+        System.out.println("8 / 2 = " + Calculator.divide(8, 2));
+
+
+
     }
 }
