0e101f368f71469a20ff788404f742a0c9e0d0f2
2107083 <deb.nath.3.1416@gmail.com>
2025-08-21 20:54:11 +0600

    Completing the second portion

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        Student s = new Student("Alice", 20);
+        System.out.println("Student Name: " + s.getName());
+        System.out.println("Student Age: " + s.getAge());
+    }
 }
